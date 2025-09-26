19c92a3f1a842e03fd7c3cad9f5cf1867352f3c8
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-09-26 14:39:45 +0530

    fix origin of reverse-proxy

--- reverse-proxy/index.js
+++ reverse-proxy/index.js
@@ -3,13 +3,22 @@ dotenv.config()
 
 import express from "express"
 import httpProxy from "http-proxy"
+import cors from 'cors'
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 9000
 const BASE_PATH = process.env.S3_URL
 
 const proxy = httpProxy.createProxyServer();
 
+const DYNAMIC_LOCALHOST_REGEX = /^https?:\/\/(.+\.)?localhost:8000/;
+
+const corsConfig = {
+    origin: DYNAMIC_LOCALHOST_REGEX,
+    credentials: true,
+};
+
+app.use(cors(corsConfig));
 app.use((req, res) => {
     const hostname = req.hostname;
     const subdomain = hostname.split('.')[0];
@@ -24,4 +33,4 @@ proxy.on('proxyReq', (proxyReq, req, res) => {
         proxyReq.path += 'index.html'
 })
 
-app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`))
+app.listen(PORT, () => console.log(`Reverse Proxy Running on Port ${PORT}`))
